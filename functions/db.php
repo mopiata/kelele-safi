1fe1020fc19a439add1853b64a5369926a64f3fb
mopiata <mopiata@safaricomdigitalacademy.co.ke>
2019-04-18 06:51:20 +0300

    new heroku cleardb configuration

--- functions/db.php
+++ functions/db.php
@@ -1,5 +1,14 @@
 <?php
-$con = mysqli_connect('localhost','root','','login_db');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$con = new mysqli($server, $username, $password, $db);
+
+//$con = mysqli_connect('localhost','root','','login_db');
 
 function row_count($result){
 
